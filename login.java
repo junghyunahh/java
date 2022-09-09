1aea3afba6895fbd99842a300ec9d8ef702ba7a4
hyunah.jung <108919634+junghyunahh@users.noreply.github.com>
2022-09-09 20:56:36 +0900

    Add files via upload

---
+++ login.java
@@ -0,0 +1,34 @@
+package login;
+public class login {
+    private String id;
+    private String password;
+    private String nickname;
+
+    public login(String id, String password, String nickname){
+        this.id = id;
+        this.password = password;
+        this.nickname = nickname;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getNickname() {
+        return nickname;
+    }
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+}
+
+
+
+
